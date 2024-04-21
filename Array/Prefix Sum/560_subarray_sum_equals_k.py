14dd2e1dc3dd0fa44a3566d77c96c9ad6c401f10
Aayush Baniyal <baniyal.aayush@tcs.com>
2024-04-21 13:15:37 +0530

    Prefix Sum + HashMap | 523 Continous Subarray sum

--- Array/Prefix Sum/560_subarray_sum_equals_k.py
+++ Array/Prefix Sum/560_subarray_sum_equals_k.py
@@ -73,3 +73,50 @@ def function_2(input_set,debug) -> bool:
         result += hash_map.get(prefix_sum % k, 0)
         hash_map[prefix_sum % k] = hash_map.get(prefix_sum % k, 0) + 1
     return result
+
+
+"""
+=======================================================523==============================================================
+Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+its length is at least two, and
+the sum of the elements of the subarray is a multiple of k.
+Note that:
+
+A subarray is a contiguous part of the array.
+An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+ 
+
+Example 1:
+
+Input: nums = [23,2,4,6,7], k = 6
+Output: true
+Explanation: [2, 4] is a continuous subarray of size 2 whose elements sum up to 6.
+Example 2:
+
+Input: nums = [23,2,6,4,7], k = 6
+Output: true
+Explanation: [23, 2, 6, 4, 7] is an continuous subarray of size 5 whose elements sum up to 42.
+42 is a multiple of 6 because 42 = 7 * 6 and 7 is an integer.
+Example 3:
+
+Input: nums = [23,2,6,4,7], k = 13
+Output: false
+"""
+
+
+def checkSubarraySum(nums, k: int) -> bool:
+    if len(nums) < 2: return False
+    hash_map, prefix_sum = {0: -1}, 0
+    for index, num in enumerate(nums):
+        prefix_sum += num
+        if k != 0: prefix_sum = prefix_sum % k
+        if prefix_sum in hash_map:
+            if index - hash_map[prefix_sum] > 1:
+                return True
+        else:
+            hash_map[prefix_sum] = index
+
+    return False
