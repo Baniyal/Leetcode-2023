76672de994853dbb9334aa0793c118d57136d674
Aayush Baniyal <baniyal.aayush@tcs.com>
2024-04-27 13:28:03 +0530

    BFS | 310 - Minimum height trees

--- Trees/BT/310_minimum_height_trees.py
+++ Trees/BT/310_minimum_height_trees.py
@@ -49,12 +49,14 @@ def function(input_arr:list)-> int:
 # OPTIMIZED SOLUTION
 
 from typing import List
+from collections import deque
+
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1:
             return [0]
 
-        # Initialize the adjacency list and degree of each node
+
         adjacency_list = defaultdict(list)
         degree = [0] * n
         for u, v in edges:
@@ -63,10 +65,10 @@ class Solution:
             degree[u] += 1
             degree[v] += 1
 
-        # Initialize leaves queue
+
         leaves = deque([i for i in range(n) if degree[i] == 1])
 
-        # Trim leaves until 2 or fewer nodes remain
+
         remaining_nodes = n
         while remaining_nodes > 2:
             leaves_count = len(leaves)
